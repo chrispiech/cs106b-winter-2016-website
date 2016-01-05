5096a47ab68e70b0a269171ff9619ca540d978b5
chrispiech <cpiech@stanford.edu>
2016-01-05 12:11:09 -0800

    a

---
+++ lectures/1-Welcome/code/guessingGame/src/guess.cpp
@@ -0,0 +1,17 @@
+#include <iostream>  // This is a true classic.
+#include "console.h" // This is a Stanford Library!
+#include "random.h"  // Another Stanford Library
+using namespace std; // The best namespace.
+
+int main() {
+
+    cout << "This is a guessing game. It's rad." << endl;
+    cout << "Try and guess my secret number. Its between ";
+    cout << "1 and 100." << endl;
+
+
+
+
+    return 0;
+}
+
---
+++ lectures/1-Welcome/code/helloWorld/src/hello.cpp
@@ -0,0 +1,13 @@
+/*
+ * The canonical hello world program in C++
+ */
+
+#include <iostream>  // This is a true classic.
+#include "console.h" // This is a Stanford Library!
+using namespace std; // The best namespace.
+
+int main() {
+
+    return 0;
+}
+
